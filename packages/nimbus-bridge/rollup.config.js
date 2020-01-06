54e053eba6e04e33526d732cde2decb053394d97
Jason Foreman <jforeman@salesforce.com>
2020-01-06 08:25:56 -0800

    Compile nimbus runtime module to es5 to support old Android versions (#112)

--- packages/nimbus-bridge/rollup.config.js
+++ packages/nimbus-bridge/rollup.config.js
@@ -1,17 +1,34 @@
 import typescript from "rollup-plugin-typescript2";
 
-export default {
-  plugins: [typescript()],
+const es5iife = {
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          target: "es5"
+        }
+      }
+    })
+  ],
   input: "src/index.ts",
   output: [
     {
       file: "dist/iife/nimbus.js",
       name: "nimbus",
       format: "iife"
-    },
+    }
+  ]
+};
+
+const es6esm = {
+  plugins: [typescript()],
+  input: "src/index.ts",
+  output: [
     {
       file: "dist/es/nimbus.js",
       format: "es"
     }
   ]
 };
+
+export default [es5iife, es6esm];
