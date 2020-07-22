09d6bdc00e0356db14f0e1b51e793ec02fceec53
Jamie Houston <30670552+jamie-houston@users.noreply.github.com>
2020-07-22 12:39:41 -0500

    added copyright (#253)

--- platforms/android/build.gradle.kts
+++ platforms/android/build.gradle.kts
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2020, Salesforce.com, inc.
+// All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+//
+
 import groovy.json.JsonSlurper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.dokka.gradle.DokkaTask
